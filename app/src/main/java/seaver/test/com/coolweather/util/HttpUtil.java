a0cecff6898298b76cc80176d25fcb89c1046cf2
zhenghaichuan <haichuanzheng@ecarx.com.cn>
2018-05-07 16:56:39 +0800

    完成遍历省市县三级列表的功能.

---
+++ app/src/main/java/seaver/test/com/coolweather/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package seaver.test.com.coolweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by zhenghaichuan on 18-5-7.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client=new OkHttpClient();
+        Request request=new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
